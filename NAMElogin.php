ea658eb3fef3b7aa2cd1bb0fda78226a503d3ab8
owl77 <41900928+owl77@users.noreply.github.com>
2023-02-10 15:09:58 +0000

    Update NAMElogin.php

--- NAMElogin.php
+++ NAMElogin.php
@@ -26,12 +26,12 @@ input[type="submit"]{
 
 
 
-if($_POST["login"]=="admin" && $_POST["pass"]=="owl123")
+if($_POST["login"]=="admin" && $_POST["pass"]=="password")
 
 {
 
 $_SESSION["auth"]="yes";
-if($_POST["login"]=="owl123"){$_SESSION["login"]="admin";}
+if($_POST["login"]=="password"){$_SESSION["login"]="admin";}
 
 
 echo("<script>window.location.href='NAMEmessenger.php'</script>");     }
